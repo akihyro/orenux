25d6d6857e42debbc3848f7a00b0812eb7295397
akihyro <akihyro@users.noreply.github.com>
2014-07-01 01:35:29 +0900

    プロンプトに git ブランチ名を表示

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -2,3 +2,10 @@
 package 'git' do
   options '--enablerepo=rpmforge-extras'
 end
+
+remote_file '/etc/profile.d/git-prompt.sh' do
+  source 'https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh'
+  checksum '1665591b64eefe3cd0804c938ed3e112fe93ecb04094e247fe60099f3afeab1b'
+end
+
+template '/etc/profile.d/ps1.sh'
