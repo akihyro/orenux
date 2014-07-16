a1c112827501fce4d1ccc88b690c93a8b588ee7e
akihyro <akihyro@users.noreply.github.com>
2014-07-16 22:37:10 +0900

    Java: アップデート (1.7.0_60=>1.7.0_65)

--- cookbooks/java/recipes/default.rb
+++ cookbooks/java/recipes/default.rb
@@ -1,17 +1,18 @@
 
 directory '/vagrant/.orenux-cache/java'
 
-bash '/vagrant/.orenux-cache/java/jdk-7u60-linux-x64.rpm' do
+bash '/vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm' do
   not_if <<-EOC
-    sha256sum /vagrant/.orenux-cache/java/jdk-7u60-linux-x64.rpm | grep -q '00bc3841127b4b569b78b07d1fb30bfe6bc483dfa72b3fa256c586a55386ed2e'
+    sha256sum /vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm | \
+        grep -q '5aea4e14ab0d650c49267a53817df67320f7619b3e29722bab3715029dd9821c'
   EOC
   code <<-EOC
-    curl -L -o /vagrant/.orenux-cache/java/jdk-7u60-linux-x64.rpm \
+    curl -L -o /vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm \
         -H 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com; oraclelicense=accept-securebackup-cookie' \
-        'http://download.oracle.com/otn-pub/java/jdk/7u60-b19/jdk-7u60-linux-x64.rpm'
+        'http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jdk-7u65-linux-x64.rpm'
   EOC
 end
 
-rpm_package 'jdk-7u60' do
-  source '/vagrant/.orenux-cache/java/jdk-7u60-linux-x64.rpm'
+rpm_package 'jdk-7u65' do
+  source '/vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm'
 end
