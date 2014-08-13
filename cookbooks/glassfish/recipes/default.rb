3299f532d22d50e545fdb3a1944caa7dd06301dd
akihyro <akihyro@users.noreply.github.com>
2014-08-13 13:53:51 +0900

    GlassFish: アップデート (4.0.1-b08=>4.0.1-b10)

--- cookbooks/glassfish/recipes/default.rb
+++ cookbooks/glassfish/recipes/default.rb
@@ -1,18 +1,18 @@
 
 directory '/vagrant/.orenux-cache/glassfish'
 
-remote_file '/vagrant/.orenux-cache/glassfish/glassfish-4.0.1-b08-ml.zip' do
-  source 'http://dlc.sun.com.edgesuite.net/glassfish/4.0.1/promoted/glassfish-4.0.1-b08-ml.zip'
-  checksum '1bdecd7cb26a4aa66bfce73106247dc1bb0d02f8e81045f58ec72b3f3938731a'
+remote_file '/vagrant/.orenux-cache/glassfish/glassfish-4.0.1-b10.zip' do
+  source 'http://dlc.sun.com.edgesuite.net/glassfish/4.0.1/promoted/glassfish-4.0.1-b10.zip'
+  checksum '11f9440739cd96aad9f0032152718682afdc69c4ce817210f8e74a106ae9a20d'
 end
 
 bash 'glassfish_extract' do
   not_if <<-EOC
-    test -d /opt/glassfish-4.0.1-b08-ml
+    test -d /opt/glassfish-4.0.1-b10
   EOC
   code <<-EOC
-    unzip -q -d /opt /vagrant/.orenux-cache/glassfish/glassfish-4.0.1-b08-ml.zip
-    mv /opt/glassfish4 /opt/glassfish-4.0.1-b08-ml
+    unzip -q -d /opt /vagrant/.orenux-cache/glassfish/glassfish-4.0.1-b10.zip
+    mv /opt/glassfish4 /opt/glassfish-4.0.1-b10
   EOC
 end
 
