a46b4b4b8b5aa77fdd2054e30bbd16794e1b917c
akihyro <akihyro@users.noreply.github.com>
2014-08-13 13:34:58 +0900

    Java: アップデート (1.7.0_65=>1.7.0_67)

--- cookbooks/java/recipes/default.rb
+++ cookbooks/java/recipes/default.rb
@@ -1,18 +1,26 @@
 
 directory '/vagrant/.orenux-cache/java'
 
-bash '/vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm' do
+bash '/vagrant/.orenux-cache/java/jdk-7u67-linux-x64.tar.gz' do
   not_if <<-EOC
-    sha256sum /vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm | \
-        grep -q '5aea4e14ab0d650c49267a53817df67320f7619b3e29722bab3715029dd9821c'
+    sha256sum /vagrant/.orenux-cache/java/jdk-7u67-linux-x64.tar.gz | \
+        grep -q '54dd1e13edf18c64941a55da9c91210b53dc5cf48f1a8f4538c863049e346335'
   EOC
   code <<-EOC
-    curl -L -o /vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm \
+    curl -L -o /vagrant/.orenux-cache/java/jdk-7u67-linux-x64.tar.gz \
         -H 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com; oraclelicense=accept-securebackup-cookie' \
-        'http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jdk-7u65-linux-x64.rpm'
+        'http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.tar.gz'
   EOC
 end
 
-rpm_package 'jdk-7u65' do
-  source '/vagrant/.orenux-cache/java/jdk-7u65-linux-x64.rpm'
+bash 'java7_extract' do
+  not_if <<-EOC
+    test -d /opt/jdk-1.7.0_67
+  EOC
+  code <<-EOC
+    tar xfz /vagrant/.orenux-cache/java/jdk-7u67-linux-x64.tar.gz -C /opt
+    mv /opt/jdk1.7.0_67 /opt/jdk-1.7.0_67
+  EOC
 end
+
+template '/etc/profile.d/java.sh'
