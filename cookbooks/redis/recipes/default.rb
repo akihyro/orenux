ef3b14d7475b07956ffa0c438eb5881e38676807
akihyro <akihyro@users.noreply.github.com>
2014-10-17 13:11:49 +0900

    Redis レシピにコメントを付与

--- cookbooks/redis/recipes/default.rb
+++ cookbooks/redis/recipes/default.rb
@@ -1,11 +1,17 @@
+#=======================================================================================================================
+# Redis
+#=======================================================================================================================
 
+# キャッシュディレクトリ
 directory '/vagrant/.orenux-cache/redis'
 
+# ダウンロード
 remote_file '/vagrant/.orenux-cache/redis/redis-2.8.6.tar.gz' do
   source 'http://download.redis.io/releases/redis-2.8.6.tar.gz'
   checksum 'efd0c9cb8d2696db44d8cb8309fed96607f68b93bb126615e64bff364e716658'
 end
 
+# 展開
 bash 'redis_extract' do
   not_if <<-EOC
     test -d /opt/redis-2.8.6
@@ -15,7 +21,8 @@ bash 'redis_extract' do
   EOC
 end
 
-bash 'redis_make' do
+# インストール
+bash 'redis_install' do
   not_if <<-EOC
     test -d /opt/redis-2.8.6/bin
   EOC
@@ -26,29 +33,31 @@ bash 'redis_make' do
   EOC
 end
 
-template '/etc/profile.d/redis.sh'
-
+# サービス起動スクリプト 修正
 # [utils/install_server.sh]
 #   => そのままインストールすると chkconfig が上手く動かないので一部書き換える
 #   => 参考: install.shがそのままでは動かないので修正する - kwyの日記
 #            http://d.hatena.ne.jp/kwy/20130925/1380120686
-bash 'redis_fix_install_server' do
+bash 'redis_fix_service_script' do
   only_if <<-EOC
     egrep -q '!`which chkconfig`' /opt/redis-2.8.6/utils/install_server.sh
   EOC
   code <<-EOC
-    cd /opt/redis-2.8.6/utils
-    sed -i -e 's/\!\`which chkconfig\`/\! \`which chkconfig\`/g' install_server.sh
+    sed -i -e 's/\!\`which chkconfig\`/\! \`which chkconfig\`/g' /opt/redis-2.8.6/utils/install_server.sh
   EOC
 end
 
-bash 'redis_install_server' do
+# 環境変数設定
+template '/etc/profile.d/redis.sh'
+
+# サービス起動
+bash 'redis_service' do
   not_if <<-EOC
     test -f /etc/init.d/redis_6379
   EOC
   code <<-EOC
     . /etc/profile.d/redis.sh
-    cd /opt/redis-2.8.6/utils
+    cd $REDIS_HOME/utils
     ./install_server.sh
   EOC
 end
