1ec9ce3092b3c99536dbcb356f549fb55614ca54
akihyro <akihyro@users.noreply.github.com>
2014-10-17 13:00:12 +0900

    Ruby バージョンアップ (2.1.2 => 2.1.3)

--- cookbooks/ruby/recipes/default.rb
+++ cookbooks/ruby/recipes/default.rb
@@ -14,45 +14,48 @@ directory '/opt/rbenv-0.4.0/plugins'
 # ruby-build ダウンロード
 git '/opt/rbenv-0.4.0/plugins/ruby-build' do
   repository 'https://github.com/sstephenson/ruby-build.git'
-  revision 'v20140702'
+  revision 'v20140926'
 end
 
 # 環境変数設定
 template '/etc/profile.d/rbenv.sh'
 
 
-# Ruby 2.1.2 インストール
-bash 'ruby_install_2.1.2' do
+# Ruby 2.1.3 インストール
+bash 'ruby_install_2.1.3' do
   not_if <<-EOC
     . /etc/profile.d/rbenv.sh
-    rbenv versions | grep -q 2.1.2
+    rbenv versions | grep -q 2.1.3
   EOC
   code <<-EOC
     . /etc/profile.d/rbenv.sh
-    rbenv install 2.1.2
+    rbenv install 2.1.3
   EOC
 end
 
-# グローバルバージョン 設定
-bash 'ruby_global_2.1.2' do
-  not_if <<-EOC
-    . /etc/profile.d/rbenv.sh
-    rbenv global | grep -q 2.1.2
-  EOC
-  code <<-EOC
-    . /etc/profile.d/rbenv.sh
-    rbenv global 2.1.2
-  EOC
-end
-
-# Ruby 2.1.2 Bundler インストール
+# Ruby 2.1.3 Bundler インストール
 bash 'ruby_install_bundler' do
   not_if <<-EOC
     . /etc/profile.d/rbenv.sh
+    rbenv shell 2.1.3
     gem list bundler | egrep "^bundler"
   EOC
   code <<-EOC
     . /etc/profile.d/rbenv.sh
+    rbenv shell 2.1.3
     gem install bundler
+  EOC
+end
+
+
+# グローバルバージョン 設定
+bash 'ruby_global_2.1.3' do
+  not_if <<-EOC
+    . /etc/profile.d/rbenv.sh
+    rbenv global | grep -q 2.1.3
+  EOC
+  code <<-EOC
+    . /etc/profile.d/rbenv.sh
+    rbenv global 2.1.3
   EOC
 end
