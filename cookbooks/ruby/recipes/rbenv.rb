59d3389cd6c1b73deac6a465d08dd5279290603b
akihyro <akihyro@users.noreply.github.com>
2014-11-18 11:46:09 +0900

    rbenv: 便利プラグインを導入

--- cookbooks/ruby/recipes/rbenv.rb
+++ cookbooks/ruby/recipes/rbenv.rb
@@ -11,10 +11,33 @@ end
 # プラグインディレクトリ
 directory '/opt/rbenv-0.4.0/plugins'
 
+# rbenv-update ダウンロード
+git '/opt/rbenv-0.4.0/plugins/rbenv-update' do
+  repository 'https://github.com/rkh/rbenv-update.git'
+  revision 'f0ff6e3264c45fdf2a8064205db6c8b3a707894e'
+end
+
 # ruby-build ダウンロード
 git '/opt/rbenv-0.4.0/plugins/ruby-build' do
   repository 'https://github.com/sstephenson/ruby-build.git'
   revision 'v20141113'
+end
+
+# rbenv-gem-rehash ダウンロード
+git '/opt/rbenv-0.4.0/plugins/rbenv-gem-rehash' do
+  repository 'https://github.com/sstephenson/rbenv-gem-rehash.git'
+  revision 'v1.0.0'
+end
+
+# rbenv-default-gems ダウンロード
+git '/opt/rbenv-0.4.0/plugins/rbenv-default-gems' do
+  repository 'https://github.com/sstephenson/rbenv-default-gems.git'
+  revision 'ead67889c91c53ad967f85f5a89d986fdb98f6fb'
+end
+
+# デフォルト gem
+template '/opt/rbenv-0.4.0/default-gems' do
+  source 'rbenv.default-gems.erb'
 end
 
 # 環境設定 (即時)
--- cookbooks/ruby/recipes/ruby.rb
+++ cookbooks/ruby/recipes/ruby.rb
@@ -5,25 +5,11 @@
 # インストール
 bash 'ruby::ruby::install' do
   not_if <<-EOC
-    eval "$(rbenv init -)"
+    eval "$(rbenv init --no-rehash -)"
     rbenv versions | grep -q 2.1.5
   EOC
   code <<-EOC
-    eval "$(rbenv init -)"
+    eval "$(rbenv init --no-rehash -)"
     rbenv install 2.1.5
-  EOC
-end
-
-# Bundler インストール
-bash 'ruby::ruby::install-bundler' do
-  not_if <<-EOC
-    eval "$(rbenv init -)"
-    rbenv shell 2.1.5
-    gem list bundler | egrep -q '^bundler'
-  EOC
-  code <<-EOC
-    eval "$(rbenv init -)"
-    rbenv shell 2.1.5
-    gem install bundler
   EOC
 end
