77a449b5b00004d3293f590d92ae619661fbced9
akihyro <akihyro@users.noreply.github.com>
2015-01-28 22:53:44 +0900

    nodejs バージョンアップ (0.10.33 => 0.10.36)

--- cookbooks/nodejs/recipes/default.rb
+++ cookbooks/nodejs/recipes/default.rb
@@ -6,29 +6,29 @@
 directory '/vagrant/.orenux-cache/nodejs'
 
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/nodejs/node-v0.10.33-linux-x64.tar.gz' do
-  source 'http://nodejs.org/dist/v0.10.33/node-v0.10.33-linux-x64.tar.gz'
-  checksum '159e5485d0fb5c913201baae49f68fd428a7e3b08262e9bf5003c1b399705ca8'
+remote_file '/vagrant/.orenux-cache/nodejs/node-v0.10.36-linux-x64.tar.gz' do
+  source 'http://nodejs.org/dist/v0.10.36/node-v0.10.36-linux-x64.tar.gz'
+  checksum '2bc13477684a9fe534bdc9d8f4a8caf6257a11953b57c42cad9b919ee259a0d5'
 end
 
 # 展開
 bash 'nodejs::extract' do
   not_if <<-EOC
-    test -d /opt/nodejs-0.10.33
+    test -d /opt/nodejs-0.10.36
   EOC
   code <<-EOC
-    tar xfz /vagrant/.orenux-cache/nodejs/node-v0.10.33-linux-x64.tar.gz -C /opt
-    mv /opt/node-v0.10.33-linux-x64 /opt/nodejs-0.10.33
+    tar xfz /vagrant/.orenux-cache/nodejs/node-v0.10.36-linux-x64.tar.gz -C /opt
+    mv /opt/node-v0.10.36-linux-x64 /opt/nodejs-0.10.36
   EOC
 end
 
 # 環境設定 (即時)
 ruby_block 'nodejs::env' do
   not_if do
-    ENV['NODEJS_HOME'] == '/opt/nodejs-0.10.33'
+    ENV['NODEJS_HOME'] == '/opt/nodejs-0.10.36'
   end
   block do
-    ENV['NODEJS_HOME'] = '/opt/nodejs-0.10.33'
+    ENV['NODEJS_HOME'] = '/opt/nodejs-0.10.36'
     ENV['PATH'] = "#{ENV['NODEJS_HOME']}/bin:#{ENV['PATH']}"
   end
 end
