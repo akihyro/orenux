50256b6797f40e48e130ee610936ef885b402661
akihyro <akihyro@users.noreply.github.com>
2015-01-28 22:55:28 +0900

    Vagrant バージョンアップ (1.6.5 => 1.7.2)

--- cookbooks/vagrant/recipes/default.rb
+++ cookbooks/vagrant/recipes/default.rb
@@ -6,10 +6,10 @@
 directory '/vagrant/.orenux-cache/vagrant'
 
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/vagrant/vagrant_1.6.5_x86_64.rpm' do
-  source 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5_x86_64.rpm'
-  checksum '90730fd10cbd811969ec58f28818685f3074f8399852dfd1d4858d75c4224fdc'
+remote_file '/vagrant/.orenux-cache/vagrant/vagrant_1.7.2_x86_64.rpm' do
+  source 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.rpm'
+  checksum '683d9926922685adfb456605ef6becaa811b87f18f54faf7c19abc4888636617'
 end
 
 # インストール
-rpm_package '/vagrant/.orenux-cache/vagrant/vagrant_1.6.5_x86_64.rpm'
+rpm_package '/vagrant/.orenux-cache/vagrant/vagrant_1.7.2_x86_64.rpm'
