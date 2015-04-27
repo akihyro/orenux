1935eb1112263e2b403ffc03fb9e158de97b9397
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-04-27 17:33:47 +0900

    Java アップデート (8u40 => 8u45)

--- cookbooks/java/recipes/default.rb
+++ cookbooks/java/recipes/default.rb
@@ -8,34 +8,34 @@ directory '/vagrant/.orenux-cache/java'
 # ダウンロード
 bash 'java::download' do
   not_if <<-EOC
-    sha256sum /vagrant/.orenux-cache/java/jdk-8u40-linux-x64.tar.gz | \
-        grep -q 'c66029684bb8aa0c0eaea7b4d21de00e36a43dcc5a82f6666d489fd27027d559'
+    sha256sum /vagrant/.orenux-cache/java/jdk-8u45-linux-x64.tar.gz | \
+        grep -q 'f298ca9239051dfddf8642fcc9e264f7fe5af10adb67027feb3a0ed0a1a2316d'
   EOC
   code <<-EOC
-    curl -L -o /vagrant/.orenux-cache/java/jdk-8u40-linux-x64.tar.gz \
+    curl -L -o /vagrant/.orenux-cache/java/jdk-8u45-linux-x64.tar.gz \
         -H 'Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com; oraclelicense=accept-securebackup-cookie' \
-        'http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.tar.gz'
+        'http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz'
   EOC
 end
 
 # 展開
 bash 'java::extract' do
   not_if <<-EOC
-    test -d /opt/jdk-1.8.0_40
+    test -d /opt/jdk-1.8.0_45
   EOC
   code <<-EOC
-    tar xfz /vagrant/.orenux-cache/java/jdk-8u40-linux-x64.tar.gz -C /opt
-    mv /opt/jdk1.8.0_40 /opt/jdk-1.8.0_40
+    tar xfz /vagrant/.orenux-cache/java/jdk-8u45-linux-x64.tar.gz -C /opt
+    mv /opt/jdk1.8.0_45 /opt/jdk-1.8.0_45
   EOC
 end
 
 # 環境設定 (即時)
 ruby_block 'java::env' do
   not_if do
-    ENV['JAVA_HOME'] == '/opt/jdk-1.8.0_40'
+    ENV['JAVA_HOME'] == '/opt/jdk-1.8.0_45'
   end
   block do
-    ENV['JAVA_HOME'] = '/opt/jdk-1.8.0_40'
+    ENV['JAVA_HOME'] = '/opt/jdk-1.8.0_45'
     ENV['PATH'] = "#{ENV['JAVA_HOME']}/bin:#{ENV['PATH']}"
   end
 end
