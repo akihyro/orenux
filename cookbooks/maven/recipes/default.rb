d47e478baaffac8f03bb1ad931e5f586dc6f5db5
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-07-22 15:11:32 +0900

    Maven アップデート v3.2.5 => v3.3.3

--- cookbooks/maven/recipes/default.rb
+++ cookbooks/maven/recipes/default.rb
@@ -6,29 +6,29 @@
 directory '/vagrant/.orenux-cache/maven'
 
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/maven/apache-maven-3.2.5-bin.tar.gz' do
-  source 'http://ftp.riken.jp/net/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz'
-  checksum '8c190264bdf591ff9f1268dc0ad940a2726f9e958e367716a09b8aaa7e74a755'
+remote_file '/vagrant/.orenux-cache/maven/apache-maven-3.3.3-bin.tar.gz' do
+  source 'http://ftp.jaist.ac.jp/pub/apache/maven/maven-3/3.3.3/binaries/apache-maven-3.3.3-bin.tar.gz'
+  checksum '3a8dc4a12ab9f3607a1a2097bbab0150c947ad6719d8f1bb6d5b47d0fb0c4779'
 end
 
 # 展開
 bash 'maven::extract' do
   not_if <<-EOC
-    test -d /opt/maven-3.2.5
+    test -d /opt/maven-3.3.3
   EOC
   code <<-EOC
-    tar xfz /vagrant/.orenux-cache/maven/apache-maven-3.2.5-bin.tar.gz -C /opt
-    mv /opt/apache-maven-3.2.5 /opt/maven-3.2.5
+    tar xfz /vagrant/.orenux-cache/maven/apache-maven-3.3.3-bin.tar.gz -C /opt
+    mv /opt/apache-maven-3.3.3 /opt/maven-3.3.3
   EOC
 end
 
 # 環境設定 (即時)
 ruby_block 'maven::env' do
   not_if do
-    ENV['M2_HOME'] == '/opt/maven-3.2.5'
+    ENV['M2_HOME'] == '/opt/maven-3.3.3'
   end
   block do
-    ENV['M2_HOME'] = '/opt/maven-3.2.5'
+    ENV['M2_HOME'] = '/opt/maven-3.3.3'
     ENV['PATH'] = "#{ENV['M2_HOME']}/bin:#{ENV['PATH']}"
   end
 end
