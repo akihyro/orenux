8d103c9331be93397f7a5d599f066de2dfc11891
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-07-22 14:55:03 +0900

    peco バージョンアップ v0.2.11 => v0.3.3

--- cookbooks/peco/recipes/default.rb
+++ cookbooks/peco/recipes/default.rb
@@ -6,23 +6,23 @@
 directory '/vagrant/.orenux-cache/peco'
 
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/peco/peco_linux_amd64-0.2.11.tar.gz' do
-  source 'https://github.com/peco/peco/releases/download/v0.2.11/peco_linux_amd64.tar.gz'
-  checksum '706f389ed6d43a5904dd75488e2d4f2901745583d5a1c6becfc0387f12588c4e'
+remote_file '/vagrant/.orenux-cache/peco/peco_linux_amd64-0.3.3.tar.gz' do
+  source 'https://github.com/peco/peco/releases/download/v0.3.3/peco_linux_amd64.tar.gz'
+  checksum '90728c44afbf63fe38cbdc54f6953c2d4187844288fb9a85f1ecd5c273e4ec94'
 end
 
 # 展開
 bash 'peco::extract' do
   not_if <<-EOC
-    test -d /opt/peco-0.2.11
+    test -d /opt/peco-0.3.3
   EOC
   code <<-EOC
-    tar xfz /vagrant/.orenux-cache/peco/peco_linux_amd64-0.2.11.tar.gz -C /opt
-    mv /opt/peco_linux_amd64 /opt/peco-0.2.11
+    tar xfz /vagrant/.orenux-cache/peco/peco_linux_amd64-0.3.3.tar.gz -C /opt
+    mv /opt/peco_linux_amd64 /opt/peco-0.3.3
   EOC
 end
 
 # 環境設定
 link '/usr/local/bin/peco' do
-  to '/opt/peco-0.2.11/peco'
+  to '/opt/peco-0.3.3/peco'
 end
