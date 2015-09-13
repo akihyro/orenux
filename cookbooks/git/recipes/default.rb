c11551d8509b53fde4f9d7becbbd635c83a8e650
Akihiro Kondo <akihyro@gmail.com>
2015-09-13 15:59:37 +0900

    git バージョンアップ (v2.4.6 => v2.5.2)

--- cookbooks/git/recipes/default.rb
+++ cookbooks/git/recipes/default.rb
@@ -6,30 +6,30 @@
 directory '/vagrant/.orenux-cache/git'
 
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/git/git-2.4.6.tar.gz' do
-  source 'https://github.com/git/git/archive/v2.4.6.tar.gz'
-  checksum '33b2f07f38d2c3e11d9b1575b6529f3081ea66dae7fa1c6d8a7b77ad38d2543a'
+remote_file '/vagrant/.orenux-cache/git/git-2.5.2.tar.gz' do
+  source 'https://github.com/git/git/archive/v2.5.2.tar.gz'
+  checksum '5130a7f21fd38f108c1c2f38e70ed7756f9942f4cd84c35823c6be2390bb45ff'
 end
 
 # 展開
 bash 'git::extract' do
   not_if <<-EOC
-    test -d /opt/git-2.4.6
+    test -d /opt/git-2.5.2
   EOC
   code <<-EOC
-    tar xfz /vagrant/.orenux-cache/git/git-2.4.6.tar.gz -C /opt
+    tar xfz /vagrant/.orenux-cache/git/git-2.5.2.tar.gz -C /opt
   EOC
 end
 
 # ビルド/インストール
 bash 'git::install' do
   not_if <<-EOC
-    test -d /opt/git-2.4.6/bin
+    test -d /opt/git-2.5.2/bin
   EOC
   code <<-EOC
-    cd /opt/git-2.4.6
+    cd /opt/git-2.5.2
     make configure
-    ./configure --prefix=/opt/git-2.4.6
+    ./configure --prefix=/opt/git-2.5.2
     make all
     make install
   EOC
@@ -38,10 +38,10 @@ end
 # 環境設定 (即時)
 ruby_block 'git::env' do
   not_if do
-    ENV['GIT_HOME'] == '/opt/git-2.4.6'
+    ENV['GIT_HOME'] == '/opt/git-2.5.2'
   end
   block do
-    ENV['GIT_HOME'] = '/opt/git-2.4.6'
+    ENV['GIT_HOME'] = '/opt/git-2.5.2'
     ENV['PATH'] = "#{ENV['GIT_HOME']}/bin:#{ENV['PATH']}"
   end
 end
@@ -51,10 +51,10 @@ template '/etc/profile.d/git.sh'
 
 # プロンプト
 link '/etc/profile.d/git-prompt.sh' do
-  to '/opt/git-2.4.6/contrib/completion/git-prompt.sh'
+  to '/opt/git-2.5.2/contrib/completion/git-prompt.sh'
 end
 
 # コマンド補完
 link '/etc/bash_completion.d/git' do
-  to '/opt/git-2.4.6/contrib/completion/git-completion.bash'
+  to '/opt/git-2.5.2/contrib/completion/git-completion.bash'
 end
