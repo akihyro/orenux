e162116f641c498cd39b7d131bb9e85c4fbb8700
Akihiro Kondo <akihyro@gmail.com>
2015-09-13 15:36:50 +0900

    jq バージョンアップ (v1.4 => v1.5)

--- cookbooks/jq/recipes/default.rb
+++ cookbooks/jq/recipes/default.rb
@@ -3,24 +3,24 @@
 #=======================================================================================================================
 
 # ディレクトリ
-directory '/opt/jq/bin' do
+directory '/opt/jq-1.5/bin' do
   recursive true
 end
 
 # ダウンロード
-remote_file '/opt/jq/bin/jq' do
-  source  'http://stedolan.github.io/jq/download/linux64/jq'
-  checksum 'b9b63aa4b3cc85df8bdca884effb69d66e9ebd717454a9e212d9423ffe3e955f'
+remote_file '/opt/jq-1.5/bin/jq' do
+  source 'https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64'
+  checksum 'c6b3a7d7d3e7b70c6f51b706a3b90bd01833846c54d32ca32f0027f00226ff6d'
   mode '0755'
 end
 
 # 環境設定 (即時)
 ruby_block 'jq::env' do
   not_if do
-    ENV['JQ_HOME'] == '/opt/jq'
+    ENV['JQ_HOME'] == '/opt/jq-1.5'
   end
   block do
-    ENV['JQ_HOME'] = '/opt/jq'
+    ENV['JQ_HOME'] = '/opt/jq-1.5'
     ENV['PATH'] = "#{ENV['JQ_HOME']}/bin:#{ENV['PATH']}"
   end
 end
