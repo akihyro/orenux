5b0bc10adc393c3b927761b1b1b1fff99633d135
Akihiro Kondo <akihyro@gmail.com>
2016-01-21 05:02:43 +0900

    [remake] Cookbook: System を実装

--- chef/cookbooks/system/recipes/locale.rb
+++ chef/cookbooks/system/recipes/locale.rb
@@ -1,18 +1,18 @@
 #=======================================================================================================================
-# Base: ロケール
+# System: ロケール
 #=======================================================================================================================
 
 # 言語/文字コード
-template '/etc/sysconfig/i18n' do
-  source 'locale.i18n.erb'
+template "/etc/sysconfig/i18n" do
+  source "locale.i18n.erb"
 end
 
 # クロック制御
-template '/etc/sysconfig/clock' do
-  source 'locale.clock.erb'
+template "/etc/sysconfig/clock" do
+  source "locale.clock.erb"
 end
 
 # タイムゾーン
-link '/etc/localtime' do
-  to '/usr/share/zoneinfo/Asia/Tokyo'
+link "/etc/localtime" do
+  to "/usr/share/zoneinfo/Asia/Tokyo"
 end
