d8fde41d4f00cc9f984941ac57bbc3cd4d9ec6ef
Akihiro Kondo <akihyro@gmail.com>
2016-01-21 12:54:24 +0900

    [remake] Cookbook: peco を実装

--- chef/cookbooks/peco/recipes/default.rb
+++ chef/cookbooks/peco/recipes/default.rb
@@ -2,27 +2,21 @@
 # peco
 #=======================================================================================================================
 
-# キャッシュディレクトリ
-directory '/vagrant/.orenux-cache/peco'
-
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/peco/peco_linux_amd64-0.3.3.tar.gz' do
-  source 'https://github.com/peco/peco/releases/download/v0.3.3/peco_linux_amd64.tar.gz'
-  checksum '90728c44afbf63fe38cbdc54f6953c2d4187844288fb9a85f1ecd5c273e4ec94'
+remote_file "#{Chef::Config['file_cache_path']}/peco-0.3.5_linux_amd64.tar.gz" do
+  source "https://github.com/peco/peco/releases/download/v0.3.5/peco_linux_amd64.tar.gz"
 end
 
 # 展開
-bash 'peco::extract' do
-  not_if <<-EOC
-    test -d /opt/peco-0.3.3
-  EOC
+bash "peco::extract" do
   code <<-EOC
-    tar xfz /vagrant/.orenux-cache/peco/peco_linux_amd64-0.3.3.tar.gz -C /opt
-    mv /opt/peco_linux_amd64 /opt/peco-0.3.3
+    tar xfz #{Chef::Config['file_cache_path']}/peco-0.3.5_linux_amd64.tar.gz -C /opt
+    mv /opt/peco_linux_amd64 /opt/peco-0.3.5
   EOC
+  not_if "test -d /opt/peco-0.3.5"
 end
 
 # 環境設定
-link '/usr/local/bin/peco' do
-  to '/opt/peco-0.3.3/peco'
+link "/usr/local/bin/peco" do
+  to "/opt/peco-0.3.5/peco"
 end
