dbe2b1a3cdec21b94df61fc06e3525cdc47d9f38
Akihiro Kondo <akihyro@gmail.com>
2016-01-21 15:36:51 +0900

    [remake] Cookbook: Vagrant を実装

--- chef/cookbooks/vagrant/recipes/default.rb
+++ chef/cookbooks/vagrant/recipes/default.rb
@@ -2,14 +2,10 @@
 # Vagrant
 #=======================================================================================================================
 
-# キャッシュディレクトリ
-directory '/vagrant/.orenux-cache/vagrant'
-
 # ダウンロード
-remote_file '/vagrant/.orenux-cache/vagrant/vagrant_1.7.4_x86_64.rpm' do
-  source 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.rpm'
-  checksum 'b0a09f6e6f9fc17b01373ff54d1f5b0dc844394886109ef407a5f1bcfdd4e304'
+remote_file "#{Chef::Config['file_cache_path']}/vagrant_1.8.1_x86_64.rpm" do
+  source "https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.rpm"
 end
 
 # インストール
-rpm_package '/vagrant/.orenux-cache/vagrant/vagrant_1.7.4_x86_64.rpm'
+rpm_package "#{Chef::Config['file_cache_path']}/vagrant_1.8.1_x86_64.rpm"
